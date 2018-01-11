1dc36c00c015d6b43b2eb804a88ae5bf1fd7db35
Ilia Idakiev <ilia.idakiev@gmail.com>
2018-01-11 10:20:50 +0200

    configured build process

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = socketIO(http);
 var port = process.env.PORT || 3000;
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 var rooms = [];
 io.on('connection', function (socketFS) {
     socketFS.on('roomName', function (msg) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const distPath = path.join(__dirname, '/dist/public');
+
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+  entry: "./src/public/game.ts",
+  output: {
+    filename: "bundle.js",
+    path: distPath
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    node: {
-        console: false,
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-      },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader", options: { configFileName: 'tsconfig.public.json' } },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Your app title',
+    template: './src/public/index.html',
+  })]
 };
